cc187f601cf8860a334f5a0f5b5a57f646b043a4
HyperManTT <randyram9@outlook.com>
2015-10-08 22:07:05 -0400

    Initial Commit

---
+++ binaryCounter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  binaryCounter
+//
+//  Created by Randy Ram on 08/10/2015.
+//  Copyright (c) 2015 Randy Ram. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
